96769d47e50ef28cffa71b1ae46440d6f0c827a6
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-11-04 22:33:15 +0300

    Исправлены недочеты

--- js/footer.js
+++ js/footer.js
@@ -1,18 +1,24 @@
 document.getElementById("contacts").innerHTML = '\
-<div class="block-content">\
-    <h2>Контакты</h2>\
-    <hr>\
-    <div id="contacts" class="block-contacts">\
-        <a target="_blank" href="https://github.com/Berliner187" title="Страница на GitHub">\
-            <img class="img-icons" src="js/social/github.png" alt="Github"></a>\
-        <a href="mailto:kozak187er@yandex.com" title="Написать на почту">\
-            <img class="img-icons" src="js/social/mail.svg" alt="Mail"></a>\
-        <a target="_blank" href="https://vk.com/hochgeschwindigkeitszug" title="Страница VK">\
-            <img class="img-icons" src="js/social/vk.png" alt="VK"></a>\
-        <a target="_blank" href="https://www.youtube.com/channel/UCKKwdEHfiAaiu1B7-yrC9SA" title="Канал на YouTube">\
-            <img class="img-icons" src="js/social/youtube.webp" alt="YouTube"></a>\
-        <a target="_blank" href="https://t.me/schneller_los" title="Написать в Telegram">\
-            <img class="img-icons" src="js/social/telegram.webp" alt="Telegram"></a>\
-        </a>\
+<div id="contacts" class="block-content">\
+        <h2>Контакты</h2>\
+        <hr>\
+        <p>\
+            Написать на почту: <a href="mailto:kozak187er@yandex.com" title="Написать на почту">kozak187er@yandex.com</a>\
+        </p>\
+        <div id="contacts" class="block-contacts">\
+            <a target="_blank" href="https://github.com/Berliner187" title="Страница на GitHub">\
+                GitHub\
+            </a>\
+            <a target="_blank" href="https://vk.com/hochgeschwindigkeitszug" title="Страница VK">\
+                Вконтакте\
+            </a>\
+            <a target="_blank" href="https://www.youtube.com/channel/UCKKwdEHfiAaiu1B7-yrC9SA" title="Канал на YouTube">\
+                YouTube\
+            </a>\
+            <a target="_blank" href="https://t.me/schneller_los" title="Написать в Telegram">\
+                Telegram\
+            </a>\
+            </a>\
+        </div>\
     </div>\
 </div>';
